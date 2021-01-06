3266d489e2ee82b562d42e8a8e0b585004835ed4
Natalia Quezada <66387884+nsquezadam@users.noreply.github.com>
2021-01-06 13:22:34 -0300

    modal de subcategorias

--- src/components/Beneficios/Canjes/PointsWins.jsx
+++ src/components/Beneficios/Canjes/PointsWins.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React from 'react'
 import logoPtos from '../../../img/logPtos.svg';
 import './styles/pointsWins.css';
 //import { ContextUser } from '../../../App';
@@ -13,15 +13,12 @@ const PointsWins = () => {
 
   return (
     <>  
-   
       <h3>Puntos Acumulados</h3>
-      
         <div className="container_point_informationsBegginer">
       <div className="containerLogPtos">
       <img src={logoPtos} alt="logoPtos"/>
       </div>
        {/* <h4>{data[user].puntos}</h4> */}
-  
       </div>
       <h5>Categoria Beginner:Hasta 50.000 Puntos</h5>
     </>
--- src/components/Beneficios/Canjes/Rewards.jsx
+++ src/components/Beneficios/Canjes/Rewards.jsx
@@ -2,10 +2,15 @@ import React, {useState} from 'react'
 import CardPartners from './CardPartners'
 import './styles/rewards.css'
 import PointsWins from  './PointsWins'
+import CardSubCategoriaAdventure from './CardSubCategoriaAdventure'
 
 
 const Rewards = () => {
-
+  const [adventureSubcat, setadventureSubcat] = useState(false)
+  // const accessCategory = () => {
+  //   console.log("btn")
+  //   setadventureSubcat(!adventureSubcat)
+  // } 
   return (
     <div className="container_rewards">
       <div className="container_rewards_components">
@@ -17,7 +22,8 @@ const Rewards = () => {
       <p>Paga con tu tarjeta MACH en cualquiera de estas tiendas y acumula más puntos. Puedes canjear tus puntos en cualquiera de las tiendas asociadas del comercio.</p>
       </div>
      
-     <CardPartners />
+      <CardPartners  click={() => setadventureSubcat(!adventureSubcat)}/>
+      <CardSubCategoriaAdventure adventureSubcat={adventureSubcat}/>
      </div>
       </div>
      
