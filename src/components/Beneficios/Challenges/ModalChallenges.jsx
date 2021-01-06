317198513d233dd9df87947ac292c4a55a80a4bd
CatalinaJL <catalina.p.jl@gmail.com>
2021-01-06 09:25:03 -0300

    Add of data in json and changes of filters in FilterChallenge. And ModalChallenge.jsx

--- src/components/Beneficios/Challenges/ModalChallenges.jsx
+++ src/components/Beneficios/Challenges/ModalChallenges.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import './Styles/ModalChallenges.css';
 
 const ModalChallenges = (props) => {
+    const divStyle ={
+        display:props.displayModal ? 'block' : 'none'
+    };
+
+    const closeModal =(e) =>{
+            e.stopPropagation()
+            props.closeModal()
+    };
+
     return (
-        <div className="Modal">
-            <div className="modal-content">
-                <span className="close">
+        <div className="Modal"
+        onClick={closeModal}
+        style={divStyle}>
+            <div className="modal-content" onClick={e => e.stopPropagation()}>
+                <span className="close" onClick={closeModal}>
                     x
                 </span>
             </div>
