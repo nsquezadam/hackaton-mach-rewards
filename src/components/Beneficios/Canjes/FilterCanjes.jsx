c7cc4e32f019466df7d91d0d58f9718b2447bdad
Natalia Quezada <66387884+nsquezadam@users.noreply.github.com>
2021-01-05 23:43:36 -0300

    filtro canjes a revisar

--- src/components/Beneficios/Canjes/FilterCanjes.jsx
+++ src/components/Beneficios/Canjes/FilterCanjes.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+/* eslint-disable no-lone-blocks */
+import React from 'react';
 import './styles/filterCanjes.css'
 import back from '../../../img/back.svg'
 
 
 const Filter = (props) => {
-  const classToogle = props.filter === true ? 'toggleVisible' : 'toggleHidden';
+  const classToogle = props.filter === true ? 'toggleVisibleCanjes' : 'toggleHiddenCanjes';
 
   const handleInputChange = (event) => {
     props.setFilterMark([...props.filterMark, event.target.value])
@@ -24,17 +25,26 @@ const Filter = (props) => {
             <span className='textClearFilter'>Limpiar</span>
           </div>
           <span className='textTitleFilterKind'>Tipo de beneficio</span>
-          <div className='containerAllFilters'>
-            <label className='eachFilterName'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox"  value="BarCafetería" /> Bar / Cafetería </label>
-            <label className='eachFilterName'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox"  value="Tecnología" /> Tecnología </label>
-            <label className='eachFilterName'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox"  value="streamin" /> streamin </label>
-            <label className='eachFilterName'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox" value="Mascotas" /> Mascotas </label>
-            <label className='eachFilterName'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox" value="Comida" /> Comida </label>
-            <label className='eachFilterName'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox" value="Planes Mobile" /> Planes Mobile </label>
-            <label className='eachFilterName'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox" value="Hogar" /> Hogar </label>
-            <label className='eachFilterName'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox"  value="Todas las categorías" /> Todas las categorías </label>
+          <div className='containerAllFiltersCanjes'>
+            <label className='eachFilterNameCanjes'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox"  value="Bar / Cafeteria" /> Bar / Cafetería </label>
+            <label className='eachFilterNameCanjes'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox"  value="Tecnologia" /> Tecnología </label>
+            <label className='eachFilterNameCanjes'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox"  value="Streaming" /> Streaming </label>
+            <label className='eachFilterNameCanjes'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox" value="Mascotas" /> Mascotas </label>
+            <label className='eachFilterNameCanjes'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox" value="Restaurant" /> Restaurant </label>
+            <label className='eachFilterNameCanjes'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox" value="Telefonía" /> Telefonía</label>
+            <label className='eachFilterNameCanjes'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox" value="Hogar" /> Hogar </label>
+            <label className='eachFilterNameCanjes'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox" value="Mascotas" /> Mascotas </label>
+            <label className='eachFilterNameCanjes'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox" value="Hoteles" /> Hoteles </label>
+            <label className='eachFilterNameCanjes'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox" value="Internacional" /> Internacional </label>
+            <label className='eachFilterNameCanjes'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox" value="Viajes" /> Viajes </label>
+            <label className='eachFilterNameCanjes'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox" value="Aventuras" /> Aventuras </label>
+            <label className='eachFilterNameCanjes'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox" value="Deportes" /> Deportes </label>
+            <label className='eachFilterNameCanjes'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox" value="Juguetería" /> Juguetería </label>
+            <label className='eachFilterNameCanjes'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox" value="Mercadería" /> Mercadería </label>
+            <label className='eachFilterNameCanjes'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox" value="Banda ancha" /> Banda ancha</label>
+            <label className='eachFilterNameCanjes'><input className='eachInputCheckbox' onChange={handleInputChange} type="checkbox"  value="Todas las categorías" /> Todas las categorías </label>
           </div>
-          <div className='containerExitFilter'>
+          <div className='containerExitFilterCanjes'>
             <span className='textClearFilter' onClick={() => props.setFilter(!props.filter)}>Salir</span>
             <button className='btnFilter' onClick={() => {{props.setFilter(!props.filter)}; enviarDatos()}}>Aplicar</button>
           </div>
--- src/components/Beneficios/Canjes/Rewards.jsx
+++ src/components/Beneficios/Canjes/Rewards.jsx
@@ -2,17 +2,9 @@ import React, {useState} from 'react'
 import CardPartners from './CardPartners'
 import './styles/rewards.css'
 import PointsWins from  './PointsWins'
-import FilterCanjes from './FilterCanjes'
-import filterIcon from '../../../img/filter.svg'
+
 
 const Rewards = () => {
-  const [filter, setFilter] = useState(false);
-  const [filterMark, setFilterMark] = useState([])
-
-  let buttonFilter=''
-// seleccion segun   usuario  beginner  o adventure
-  // const classContainerDiscount = user === 0 ? 'containerDiscountBegginer' : 'containerDiscountAdventure';
-  // const classTextGetCoupon = user === 0 ? 'textGetCouponBegginer' : 'textGetCouponAdventure';
 
   return (
     <div className="container_rewards">
@@ -24,22 +16,12 @@ const Rewards = () => {
       <h5>Canjea tus Puntos </h5>
       <p>Paga con tu tarjeta MACH en cualquiera de estas tiendas y acumula más puntos. Puedes canjear tus puntos en cualquiera de las tiendas asociadas del comercio.</p>
       </div>
-      <div className='containerFiltersCanjes'>
-          <div className='containerButtonsFilter'>
-            {buttonFilter}
-          </div>
-          <img src={filterIcon} alt='filter' onClick={() => setFilter(!filter)} />
-        </div>
-      <div>
-        < FilterCanjes filter={filter} setFilter={setFilter} filterMark={filterMark} setFilterMark={setFilterMark} />
-      </div>
-    
-      <div className="containerCanjes">
+     
      <CardPartners />
      </div>
       </div>
      
-      </div>
+    
 
   )
 }
